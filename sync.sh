09af239136307b2aa894b58db3c0ddba4c39ef2b
ash-hashtag <hashtag438@gmail.com>
2025-01-05 21:15:31 +0530

    synced Sun Jan  5 09:15:31 PM IST 2025

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-cp -r ~/.tmux.conf ~/.config/helix ~/.config/alacritty ~/.config/wezterm/wezterm.lua ~/.bashrc ./
+cp -r ~/.tmux.conf ~/.config/helix ~/.config/alacritty ~/.config/wezterm ~/.bashrc ./
 git add .
 git commit -m "synced $(date)"
 git push -u origin main
